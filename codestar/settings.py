7ef4c18f398dd1f91b1ebf4bf6d1ca9b1e622676
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-11-27 20:28:49 +0000

    feat: admin complete

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['agaeta-django-blog-f4f82cf29d22.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['agaeta-django-blog-f4f82cf29d22.herokuapp.com',
+                 os.environ.get('localhost')]
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
@@ -135,7 +137,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaClodinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
